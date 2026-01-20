6ee13756e0b823ea342fb76c24ee39299c274cfc
Ольга <hellbor@mail.ru>
2026-01-20 21:00:25 +0300

    Numbers

--- src/Main.kt
+++ src/Main.kt
@@ -8,4 +8,17 @@ fun main() {
         }
     }
     println()
+
+
+
+    val numbers = arrayOf(2,7,-3,0,17,-11,20,15,-9)
+    for(i in numbers) {
+        val category = when {
+            i < 0 -> "negative"
+            i == 0 -> "zero"
+            i in 1.. 9 -> "small"
+            else -> "big"
+        }
+        println("$numbers -> $category")
+    }
 }
